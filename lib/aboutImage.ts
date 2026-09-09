1629503d65764962bcd775e7b4244c36223251a9
baltutis02-crypto <baltutis02@gmail.com>
2026-09-09 22:21:17 +0500

    Assemble Nastya about art as data URI

---
+++ lib/aboutImage.ts
@@ -0,0 +1,10 @@
+import p0 from "./aboutImageChunks/p0";
+import p1 from "./aboutImageChunks/p1";
+import p2 from "./aboutImageChunks/p2";
+import p3 from "./aboutImageChunks/p3";
+import p4 from "./aboutImageChunks/p4";
+import p5 from "./aboutImageChunks/p5";
+import p6 from "./aboutImageChunks/p6";
+import p7 from "./aboutImageChunks/p7";
+
+export const aboutImage = `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}`;
