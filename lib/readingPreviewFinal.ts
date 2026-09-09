2b5824b4768e80d90bda9ab04b90600d95d03047
baltutis02-crypto <baltutis02@gmail.com>
2026-09-10 00:07:14 +0500

    Assemble final embedded reading preview image

---
+++ lib/readingPreviewFinal.ts
@@ -0,0 +1,13 @@
+import p0 from "./readingPreviewFinalChunks/p0";
+import p1 from "./readingPreviewFinalChunks/p1";
+import p2 from "./readingPreviewFinalChunks/p2";
+import p3 from "./readingPreviewFinalChunks/p3";
+import p4 from "./readingPreviewFinalChunks/p4";
+import p5 from "./readingPreviewFinalChunks/p5";
+import p6 from "./readingPreviewFinalChunks/p6";
+import p7 from "./readingPreviewFinalChunks/p7";
+import p8 from "./readingPreviewFinalChunks/p8";
+import p9 from "./readingPreviewFinalChunks/p9";
+import p10 from "./readingPreviewFinalChunks/p10";
+
+export const readingPreviewFinal = `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}${p8}${p9}${p10}`;
